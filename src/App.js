7dff5c9b4e6e0af25bf9e215e4260c150db7d557
edupoly <info.edupoly@gmail.com>
2023-09-25 20:24:09 +0530

    masnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Counter from './Counter'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:'1px solid',margin:'10px',padding:'10px'}}>
+      <h1>Welcome to React App Development by Praveen Gubbala</h1>
+      <Counter></Counter>
     </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Counter(){
+    var [c,setC]=React.useState(10)
+    return(
+        <div style={{border:'1px solid',margin:'10px',padding:'10px'}}>
+            <h1>Counter:{c}</h1>
+            <button>Increment</button>
+            <button>Decrement</button>
+        </div>
+    )
+}
+export default Counter;
