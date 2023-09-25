eb8cd2aad803b43cdea9710eefe16ab68aca5219
edupoly <info.edupoly@gmail.com>
2023-09-25 21:05:12 +0530

    sdf

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 function Todolist() {
+  var [todos,setTodos]=React.useState(["cleaning car","get water bottles","pay manu bus fee"])
+  function addTodo(){
+    var nt = document.getElementById("d1").value;
+    setTodos([...todos,nt])
+  }
   return (
     <div style={{border:'1px solid',padding:'10px',margin:'10px'}}>
         <h1>Todolist</h1>
+        <input type="text" id='d1'/>
+        <button onClick={()=>{addTodo()}}>Add Todo</button>
+        <ul>
+          {
+            todos.map((todo)=>{
+              return <li>
+                {todo}
+                </li>
+            })
+          }
+        </ul>
+       
     </div>
   )
 }
