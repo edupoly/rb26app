8d70b73d98439e95ba63a54236f3ca0ccba765d3
edupoly <info.edupoly@gmail.com>
2023-09-25 20:41:34 +0530

    sdf

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Todolist from './Todolist';
+
+function App() {
+  return (
+    <div style={{border:'1px solid',padding:'10px',margin:'10px'}}>
+      <h1>THIS IS APP</h1>
+      <Todolist></Todolist>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todolist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todolist() {
+  return (
+    <div style={{border:'1px solid',padding:'10px',margin:'10px'}}>
+        <h1>Todolist</h1>
+    </div>
+  )
+}
+
+export default Todolist
